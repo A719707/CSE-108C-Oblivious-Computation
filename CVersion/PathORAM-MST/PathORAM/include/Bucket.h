4b151873f617711586132d2bc0741c673778366a
A719707 <aandhare@ucsc.edu>
2025-03-21 15:50:46 -0700

    Update Bucket.h
    Static Variables:
    
    is_init: Tracks whether the bucket size has been initialized.
    
    max_size: Stores the maximum number of blocks a bucket can hold.
    
    Constructors:
    
    Default constructor: Initializes an empty bucket.
    
    Copy constructor: Creates a deep copy of another bucket.
    
    Block Management:
    
    getBlockByIndex(): Retrieves a block by its index.
    
    addBlock(): Adds a block to the bucket.
    
    removeBlock(): Removes a block from the bucket.
    
    Static Methods:
    
    setMaxSize(): Sets the maximum size of the bucket.
    
    getMaxSize(): Returns the maximum size of the bucket.
    
    resetState(): Resets the bucket's initialization state.
    
    Print Function:
    
    printBlocks(): Prints all blocks in the bucket.

--- CVersion/PathORAM-MST/PathORAM/include/Bucket.h
+++ CVersion/PathORAM-MST/PathORAM/include/Bucket.h
@@ -1,34 +1,40 @@
-//
-//
-//
-
-#ifndef PORAM_BUCKET_H
-#define PORAM_BUCKET_H
-
+#ifndef BUCKET_H
+#define BUCKET_H
 
 #include "Block.h"
 #include <vector>
 #include <stdexcept>
 
-class Bucket {
+using namespace std;
 
+class Bucket {
 public:
+    // Static variables
+    static bool is_init;
+    static int max_size;
+
+    // Constructors
     Bucket();
     Bucket(Bucket *other);
+
+    // Block management
     Block getBlockByIndex(int index);
     void addBlock(Block new_blk);
     bool removeBlock(Block rm_blk);
+
+    // Getter for blocks
     vector<Block> getBlocks();
+
+    // Static methods for bucket size
     static void setMaxSize(int maximumSize);
-    static void resetState();
     static int getMaxSize();
+    static void resetState();
+
+    // Print function
     void printBlocks();
 
 private:
-    static bool is_init; //should be initially false
-    static int max_size; //should be initially -1
-    vector<Block> blocks;
+    vector<Block> blocks;  // Blocks stored in the bucket
 };
 
-
-#endif //PORAM_BUCKET_H
+#endif // BUCKET_H
