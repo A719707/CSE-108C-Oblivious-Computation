4d4a516bf0cf9ec268c670231727dd65ca0c906a
A719707 <aandhare@ucsc.edu>
2025-03-21 15:37:17 -0700

    Update Block.cpp
     Stores multiple leaf positions
    
    Uses an array leaf_ids[MAX_ORAMS] to track where the block is located in all sub-ORAMs.
     Updated constructor to accept multiple ORAM positions
    
    Instead of a single leaf_id, it now takes an array leaf_ids[] containing one leaf position per ORAM.
    Improved print function
    
    Displays leaf positions for all sub-ORAMs instead of just one.

--- CVersion/PathORAM-MST/PathORAM/src/Block.cpp
+++ CVersion/PathORAM-MST/PathORAM/src/Block.cpp
@@ -1,6 +1,4 @@
-//
-//
-//
+
 #include "Block.h"
 #include <iostream>
 #include <string>
@@ -8,28 +6,40 @@
 
 using namespace std;
 
-Block::Block(){//dummy index
-    this->leaf_id = -1;
+// Default constructor for a dummy block
+Block::Block() {
     this->index = -1;
+    for (int i = 0; i < MAX_ORAMS; i++) {
+        this->leaf_ids[i] = -1; // Initialize all ORAM leaf positions as invalid
+    }
 }
 
-Block::Block(int leaf_id, int index, int data[]) : leaf_id(leaf_id), index(index)
-{
-   for (int i = 0; i < BLOCK_SIZE; i++){
-       this->data[i] = data[i];
-   }
+// Constructor with data and multiple ORAM leaf positions
+Block::Block(int index, int data[], int leaf_ids[]) : index(index) {
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        this->data[i] = data[i];
+    }
+    for (int i = 0; i < MAX_ORAMS; i++) {
+        this->leaf_ids[i] = leaf_ids[i]; // Store leaf positions for all sub-ORAMs
+    }
 }
 
-Block::~Block()
-{
-    //dtor
-}
+// Destructor
+Block::~Block() {}
 
-void Block::printBlock(){
-	string data_holder = "";
-	for (int i = 0; i<BLOCK_SIZE; i++) {
-		data_holder += to_string(this->data[i]);
-		data_holder += " ";
-	}
-	cout << "index: " << to_string(this->index) << " leaf id: " << to_string(this->leaf_id) << " data: " << data_holder << endl;
+// Print block information
+void Block::printBlock() {
+    string data_holder = "";
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        data_holder += to_string(this->data[i]) + " ";
+    }
+
+    cout << "index: " << to_string(this->index) << " leaf_ids: ";
+    
+    // Print leaf positions for all sub-ORAMs
+    for (int i = 0; i < MAX_ORAMS; i++) {
+        cout << leaf_ids[i] << " ";
+    }
+
+    cout << " data: " << data_holder << endl;
 }
