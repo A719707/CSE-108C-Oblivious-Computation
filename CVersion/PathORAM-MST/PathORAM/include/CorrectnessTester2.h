0eef6d41e8cca1d2b2bf766f6abf9a6d1e3bf2fb
A719707 <aandhare@ucsc.edu>
2025-03-21 22:07:32 -0700

    Update CorrectnessTester2.h

--- CVersion/PathORAM-MST/PathORAM/include/CorrectnessTester2.h
+++ CVersion/PathORAM-MST/PathORAM/include/CorrectnessTester2.h
@@ -1,18 +1,19 @@
 
+#ifndef CORRECTNESSTESTER2_H
+#define CORRECTNESSTESTER2_H
 
 #include "OramInterface.h"
-#include "OramReadPathEviction.h"
-#include "UntrustedStorageInterface.h"
-#include "ServerStorage.h"
-
-#include <vector>
-
+#include "rORAM.h"
+#include <string>
 
 class CorrectnessTester2 {
-	public:
+public:
+    CorrectnessTester2();
+    void runCorrectnessTest();
+    void runRORAMCorrectnessTest(); // New function for testing rORAM
 
-		CorrectnessTester2();
-		int* sampleData(int i);
-		void runCorrectnessTest();
-
+private:
+    int* sampleData(int i); // Helper function to generate sample data
 };
+
+#endif // CORRECTNESSTESTER2_H
